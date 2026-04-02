282a7cb2f0b2d8a09378aa6c7dc51291366f25d7
Shafaat Mursleen <shafaatmursleen@gmail.com>
2026-04-02 15:04:33 +0500

    Updated jdk version to 17

--- cropper/build.gradle.kts
+++ cropper/build.gradle.kts
@@ -16,7 +16,7 @@ licensee {
 
 kotlin {
   jvmToolchain {
-    languageVersion.set(JavaLanguageVersion.of(11))
+    languageVersion.set(JavaLanguageVersion.of(17))
   }
 }
 
@@ -34,8 +34,8 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
 
   testOptions {
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 kotlin {
   jvmToolchain {
-    languageVersion.set(JavaLanguageVersion.of(11))
+    languageVersion.set(JavaLanguageVersion.of(17))
   }
 }
 
@@ -29,8 +29,8 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
 
   buildTypes {
